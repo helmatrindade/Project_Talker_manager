bfb54e2288e868c508d66abe39367404de1245b6
helma trindade <helmacaires28@gmail.com>
2023-04-14 17:51:49 -0300

    Requisito 1 - endpoint get/talker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(express.json());
+
+const talkerPath = path.resolve(__dirname, './talker.json');
+
+const readTalker = async () => {
+  try {
+    const data = await fs.readFile(talkerPath);
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(`Arquivo não pôde ser lido: ${error}`);
+  }
+};
 
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
@@ -9,6 +22,20 @@ const PORT = process.env.PORT || '3001';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res) => {
+  // const talkers = await readTalker();
+  // if (!talkers) {
+  //   return res.status(200).json([]);
+  // } 
+  // return res.status(200).json(talkers);
+  try {
+    const talker = await readTalker();
+    return res.status(200).json(talker);
+  } catch (error) {
+    return res.status(200).send([]);
+  }
 });
 
 app.listen(PORT, () => {
