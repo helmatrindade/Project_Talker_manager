ef3ebc7a16aeb987d3443c966b0c66dbf5d30e12
helma trindade <helmacaires28@gmail.com>
2023-04-14 19:55:20 -0300

    correção lint

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ app.get('/talker/:id', async (req, res) => {
     const { id } = req.params;
     const talkers = await readTalker();
     const talker = talkers.find((t) => t.id === Number(id));
-    if (!talker) return res.status(404).json({message: "Pessoa palestrante não encontrada" });
+    if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
     return res.status(200).json(talker);
   } catch (error) {
     console.error(error);
